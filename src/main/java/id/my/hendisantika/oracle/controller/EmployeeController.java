96e242ebade6d9b7bc6ca7e0c78dd094495f2873
hendisantika <hendisantika@yahoo.co.id>
2024-09-06 03:56:00 +0700

    Add one in EmployeeController🫸🌀✏️📗🐧🐳`

--- src/main/java/id/my/hendisantika/oracle/controller/EmployeeController.java
+++ src/main/java/id/my/hendisantika/oracle/controller/EmployeeController.java
@@ -1,9 +1,11 @@
 package id.my.hendisantika.oracle.controller;
 
 import id.my.hendisantika.oracle.entity.Employee;
+import id.my.hendisantika.oracle.exception.EmployeeNotFoundException;
 import id.my.hendisantika.oracle.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,5 +34,11 @@ class EmployeeController {
     @PostMapping("/employees")
     Employee newEmployee(@RequestBody Employee newEmployee) {
         return employeeRepository.save(newEmployee);
+    }
+
+    @GetMapping("/employees/{id}")
+    Employee one(@PathVariable Long id) {
+        return employeeRepository.findById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException(id));
     }
 }

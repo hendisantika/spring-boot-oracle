d05c64b98e8c1b0448428c26754723a76103023f
hendisantika <hendisantika@yahoo.co.id>
2024-09-06 03:52:40 +0700

    Add EmployeeNotFoundException🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/oracle/exception/EmployeeNotFoundException.java
@@ -0,0 +1,20 @@
+package id.my.hendisantika.oracle.exception;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-oracle
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/09/24
+ * Time: 03.52
+ * To change this template use File | Settings | File Templates.
+ */
+class EmployeeNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 7768215094663842819L;
+
+    EmployeeNotFoundException(Long id) {
+        super("Could not find employee " + id);
+    }
+}

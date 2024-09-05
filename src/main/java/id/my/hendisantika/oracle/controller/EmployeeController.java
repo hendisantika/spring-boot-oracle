29b22d5cf864e5d6979d39f2e93fbbb2bb4b2903
hendisantika <hendisantika@yahoo.co.id>
2024-09-06 03:56:39 +0700

    Add deleteEmployee in EmployeeController🫸🌀✏️📗🐧🐳`

--- src/main/java/id/my/hendisantika/oracle/controller/EmployeeController.java
+++ src/main/java/id/my/hendisantika/oracle/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import id.my.hendisantika.oracle.entity.Employee;
 import id.my.hendisantika.oracle.exception.EmployeeNotFoundException;
 import id.my.hendisantika.oracle.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,5 +56,10 @@ class EmployeeController {
                     newEmployee.setId(id);
                     return employeeRepository.save(newEmployee);
                 });
+    }
+
+    @DeleteMapping("/employees/{id}")
+    void deleteEmployee(@PathVariable Long id) {
+        employeeRepository.deleteById(id);
     }
 }

4e9e3471dd6b043454c00f9127f7348c7c0075ec
hendisantika <hendisantika@yahoo.co.id>
2024-09-06 03:53:59 +0700

    Add EmployeeController🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/oracle/controller/EmployeeController.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.oracle.controller;
+
+import id.my.hendisantika.oracle.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-oracle
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/09/24
+ * Time: 03.53
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequiredArgsConstructor
+class EmployeeController {
+
+    private final EmployeeRepository employeeRepository;
+}
